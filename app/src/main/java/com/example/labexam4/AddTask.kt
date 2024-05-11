300c309e30f8816955c2c6ed27934b868d26eb31
IT22602114 <it22602114@my.sliit.lk>
2024-05-11 14:47:11 +0530

    AddTask comments

--- app/src/main/java/com/example/labexam4/AddTask.kt
+++ app/src/main/java/com/example/labexam4/AddTask.kt
@@ -8,20 +8,31 @@ import com.example.labexam4.databinding.ActivityMainBinding
 
 class AddTask : AppCompatActivity() {
 
+    //declare binding variable
     private lateinit var binding: ActivityAddTaskBinding
+
+    //declare a variable for db helper
     private lateinit var db: TaskDbHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Inflate the layout
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Initialize db helper
         db = TaskDbHelper(this)
 
         binding.saveButton.setOnClickListener {
+            //retrieve task name and content
             val task = binding.taskEditText.text.toString()
             val content = binding.contentEditText.text.toString()
+
+            //create task object
             val t = Task(0, task, content)
+
+            //insert task object to db
             db.insertTask(t)
             finish()
             Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
