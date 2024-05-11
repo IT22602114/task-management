d71c74f09f87c9f0a28a989d253cbae0fe13207b
IT22602114 <it22602114@my.sliit.lk>
2024-05-11 12:23:49 +0530

    complete read function

--- app/src/main/java/com/example/labexam4/TasksAdapter.kt
+++ app/src/main/java/com/example/labexam4/TasksAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.labexam4
 
-class TasksAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TasksAdapter (private var tasks:List<Task>, context: Context) : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>(){
+
+    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val taskTextView: TextView = itemView.findViewById(R.id.taskTextView)
+        val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = tasks.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val t = tasks[position]
+        holder.taskTextView.text = t.task
+        holder.contentTextView.text = t.content
+    }
+
+    fun refreshData(newTasks: List<Task>){
+        tasks = newTasks
+        notifyDataSetChanged()
+    }
 }
